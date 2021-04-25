fa4a7bea6ef83bbc28bf33b1405223b3a9956417
Felipe Oliveira <felipeoliveira.contato@gmail.com>
2021-04-25 13:27:37 -0300

    Criacao do content header + selectInput

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react'; 
+import { Container } from './styles';
+import ContentHeader from '../../components/ContentHeader';
+
+import SelectInput from '../../components/SelectInput';
 
 const Dashboard: React.FC = () => {
+
+    const options = [
+        {value: 'Rogrido', label: 'Oliveira'},
+        {value: 'Rogrido', label: 'Oliveira'},
+        {value: 'Rogrido', label: 'Oliveira'}
+    ]
+
     return(
-        <h1>Dashboard</h1>
+        <Container>
+            <ContentHeader title="Dashboard" lineColor="#000000">
+                <SelectInput options={options} />
+            </ContentHeader>
+        </Container>
     );
 }
 
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react'; 
+import { Container } from './styles';
+import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
 
 const List: React.FC = () => {
+
+    const options = [
+        {value: 'Rogrido', label: 'Oliveira'},
+        {value: 'Rogrido', label: 'Oliveira'},
+        {value: 'Rogrido', label: 'Oliveira'}
+    ]
+
     return(
-        <h1>List</h1>
+        <Container>
+        <ContentHeader title="Listagem" lineColor="#ffffff">
+            <SelectInput options={options} />
+        </ContentHeader>
+    </Container>
     );
 }
 
