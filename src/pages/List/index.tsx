df6452027dbff92d7104ed332b1bab0042c16d0d
Felipe Oliveira <felipeoliveira.contato@gmail.com>
2021-04-26 09:26:10 -0300

    Update Reade ME

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,11 @@
-import React, { useMemo } from 'react'; 
+import React, { useMemo, useState, useEffect } from 'react'; 
 import { Container, Content, Filters } from './styles';
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
+
+import gains from '../../repositories/gains'; 
+import expenses from '../../repositories/expenses'; 
 
 interface IRouteParams {
     match: {
@@ -12,7 +15,18 @@ interface IRouteParams {
     }
 }
 
+interface IData {
+    id: string;
+    description: string;
+    amountFormatted: string;
+    frequency: string;
+    dateFormatted: string;
+    tagColor: string;
+} 
+
 const List: React.FC<IRouteParams> = ({ match }) => {
+    const [data, setData] = useState<IData>([]);
+
     const { type } = match.params;
 
     const title = useMemo(() => {
@@ -22,6 +36,10 @@ const List: React.FC<IRouteParams> = ({ match }) => {
     const lineColor = useMemo(() => {
         return type === 'entry-balance' ? '#F7931B' : '#E44C4E'
     },[type]);
+
+    const listData = useMemo(() => {
+        return type === 'entry-balance' ? gains : expenses;
+    },[type])
 
     const months = [
         {value: 7, label: 'Julho'},
@@ -34,6 +52,19 @@ const List: React.FC<IRouteParams> = ({ match }) => {
         {value: 2020, label: '2020'},
         {value: 2021, label: '2021'}
     ]
+
+    useEffect(() => {
+        listData.map(item => {
+            return {
+                description: item.description,
+                amountFormatted: item.amount,
+                frequency: item.frequency,
+                dateFormatted: item.date,
+                tagColor: '#4e41f0',
+            }
+        })
+        setData(listData);
+    },[]);
 
     return(
         <Container>
